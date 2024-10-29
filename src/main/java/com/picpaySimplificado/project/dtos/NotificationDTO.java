acb8c878be5d29ef56697046c5336e6d07373f91
Bruno Souza <brunocloud78@gmail.com>
2024-10-29 17:31:57 -0300

    Notification service

---
+++ src/main/java/com/picpaySimplificado/project/dtos/NotificationDTO.java
@@ -0,0 +1,5 @@
+package com.picpaySimplificado.project.dtos;
+
+public record NotificationDTO(String email, String mensage) {
+
+}
---
+++ src/main/java/com/picpaySimplificado/project/services/NotificationService.java
@@ -0,0 +1,29 @@
+package com.picpaySimplificado.project.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.picpaySimplificado.project.dtos.NotificationDTO;
+import com.picpaySimplificado.project.entites.User;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public void sendNotification(User user, String mensage) throws Exception {
+		String email = user.getEmail();
+		NotificationDTO notificationRequest = new NotificationDTO(email, mensage);
+		
+		ResponseEntity<String> notificationResponse = 
+				restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+	
+		if (!(notificationResponse.getStatusCode() ==  HttpStatus.OK)) {
+			throw new Exception("Serviço de notificação fora do ar");
+		}
+	}
+}
